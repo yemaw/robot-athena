4651d96564366f702f8a961db4999b5f683976ab
yemaw <yemaw.online@gmail.com>
2019-08-18 01:35:14 +0800

    basic roaming mode

--- main.ts
+++ main.ts
@@ -69,7 +69,7 @@ class ThingSpeak {
 
         this.busy = true
 
-        basic.showIcon(IconNames.SmallSquare)
+        // basic.showIcon(IconNames.SmallSquare)
         if (!ESP8266ThingSpeak.isWifiConnected()) {
             ESP8266ThingSpeak.connectWifi(
                 PIN_ESP8266_RX,
@@ -81,28 +81,27 @@ class ThingSpeak {
         }
 
         if (!ESP8266ThingSpeak.isWifiConnected()) {
-            basic.showIcon(IconNames.Ghost)
-        } else {
-            basic.showIcon(IconNames.Square)
-        }
-
-        ESP8266ThingSpeak.connectThingSpeak(
-            "api.thingspeak.com",
-            THINGSPEAK_ENV_CHANNEL,
-            fields[0],
-            fields[1],
-            fields[2],
-            fields[3],
-            fields[4],
-            fields[5],
-            fields[6],
-            fields[7],
-        )
-
-        if (ESP8266ThingSpeak.isLastUploadSuccessful()) {
-            basic.showIcon(IconNames.Yes)
-        } else {
             basic.showIcon(IconNames.No)
+        } else {
+            ESP8266ThingSpeak.connectThingSpeak(
+                "api.thingspeak.com",
+                THINGSPEAK_ENV_CHANNEL,
+                fields[0],
+                fields[1],
+                fields[2],
+                fields[3],
+                fields[4],
+                fields[5],
+                fields[6],
+                fields[7],
+            )
+
+            if (ESP8266ThingSpeak.isLastUploadSuccessful()) {
+                //basic.showIcon(IconNames.Yes)
+                basic.clearScreen()
+            } else {
+                basic.showIcon(IconNames.Ghost)
+            }
         }
 
         this.busy = false
@@ -110,7 +109,39 @@ class ThingSpeak {
 
 }
 
+class Wheels {
 
+    busy: boolean
+    constructor() {
+        this.busy = false
+    }
+    forward(speed: number) {
+        this.busy = true
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Math.constrain(speed, 0, 255))
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Math.constrain(speed, 0, 255))
+    }
+    turnLeft(speed: number) {
+        this.busy = true
+        // motor.motorStop(motor.Motors.M4)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Math.constrain(speed, 0, 255))
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Math.constrain(speed, 0, 255))
+    }
+    turnRight(speed: number) {
+        this.busy = true
+        // motor.motorStop(motor.Motors.M1)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Math.constrain(speed, 0, 255))
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Math.constrain(speed, 0, 255))
+    }
+    backward(speed: number) {
+        this.busy = true
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, Math.constrain(speed, 0, 255))
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, Math.constrain(speed, 0, 255))
+    }
+    stop() {
+        motor.motorStopAll()
+        this.busy = false
+    }
+}
 
 //----------- Global Variables and Communication Keys -----------------------------
 // let DATA_DISTANCE_FRONT: number = null
@@ -259,10 +290,9 @@ class MB5VController extends Controller {
             //Distance Reading
             Data[DISTANCE_FRONT] = sonar.ping(PIN_SONAR_TRIGGER, PIN_SONAR_ECHO, PingUnit.Centimeters)
 
-            if (Data[DISTANCE_FRONT] <= 15) {
+            if (Data[DISTANCE_FRONT] <= 50) {
                 thisObj.sendNetworkData(DISTANCE_FRONT, Data[DISTANCE_FRONT])
             }
-
 
             if (Data[DISTANCE_FRONT] === null) {
                 ledEars.off()
@@ -271,7 +301,7 @@ class MB5VController extends Controller {
             } else if (Data[DISTANCE_FRONT] <= 10) {
                 ledEars.blue()
             } else {
-                ledEars.random()
+                ledEars.green()
             }
 
 
@@ -290,28 +320,24 @@ class MB5VController extends Controller {
 }
 
 class MB3VController extends Controller {
+
+    send_to_thingspeak: boolean
+
     constructor() {
         super()
 
         let thisObj = this
 
+        this.send_to_thingspeak = false
+
         let thinkSpeak = new ThingSpeak()
+        let wheels = new Wheels()
 
-        //60s
-        this.addOnEveryHook(60000, function () {
+        motor.motorStopAll()
 
-        })
-
-
-        //30s
-        this.addOnEveryHook(30000, function () {
-
-        })
-
-
-        //15s
+        // ThingSpeak
         this.addOnEveryHook(15000, function () {
-            if (!thinkSpeak.busy) {
+            if (!thinkSpeak.busy && thisObj.send_to_thingspeak) {
                 thinkSpeak.sendData(THINGSPEAK_ENV_CHANNEL, [
                     Data[TEMPERATURE],
                     Data[HUMIDITY],
@@ -340,7 +366,8 @@ class MB3VController extends Controller {
 
         //1s
         this.addOnEveryHook(1000, function () {
-
+            // let degrees = input.compassHeading();
+            // basic.showNumber(degrees)
         })
 
 
@@ -350,9 +377,46 @@ class MB3VController extends Controller {
         })
 
 
-        //100ms
-        this.addOnEveryHook(100, function () {
+        //Wheels Control
+        const w_interval = 10
+        let distance_error = 0
+        let wheels_pause_for = 0
 
+        let turning = false
+        this.addOnEveryHook(w_interval, function () {
+
+            let df = Data[DISTANCE_FRONT]
+
+            if (Data[FLAME] < 800) {
+                wheels.backward(200)
+            } else {
+                if (df == 0) {
+                    wheels.stop()
+                } else if (df > 80) {
+                    wheels.forward(255)
+                } else if (df > 50) {
+                    wheels.forward(200)
+                } else if (df > 20) {
+                    wheels.forward(150)
+                } else if (df < 10) {
+                    wheels.backward(200)
+                } else {
+                    //motor.motorStopAll()
+                    // if(!turning){
+                    //     turning = true
+                    //     let left_or_right = 1
+                    //     if(left_or_right == 1){
+                    //         wheels.turnLeft(150)
+                    //     }
+                    // }
+
+                    Math.randomRange(1, 10) < 9 ? wheels.turnLeft(130) : wheels.stop()
+                }
+            }
+
+            // distance_error = (df == 0) ? distance_error + w_interval : 0
+
+            // basic.showNumber(Math.round(distance_error / 100))
         })
 
 
